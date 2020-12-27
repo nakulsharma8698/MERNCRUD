1608a84e71a252550ef4ad69516ef9d8d80c4da2
Nakul Sharma <nakulsharma8698@gmail.com>
2020-12-27 22:37:51 +0530

    Update api.js

--- api.js
+++ api.js
@@ -109,5 +109,8 @@ connection.once('open', function() {
   {
     app.use(express.static('client/build'));
   }
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
